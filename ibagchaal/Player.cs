f85215a818ee973cfb727d0c69a768ba6d831884
dr kusu pro2000 <dr.kusu.pro2000@gmail.com@7b603418-8d72-82e7-18f8-41cf01e9d598>
2010-06-05 11:03:41 +0000

    Changing board model..

---
+++ ibagchaal/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ibagchaal
+{
+    class Player
+    {
+        protected int xPos, yPos;
+        protected BoardModel myParent;
+
+        public Player(int xP, int yP, BoardModel parent)
+        {
+            xP = xPos;
+            yP = yPos;
+            myParent = parent;
+        }
+
+        public int getXPos()
+        {
+            return xPos;
+        }
+
+        public int getYPos()
+        {
+            return yPos;
+        }
+
+        public void setPos(int xp,int yp)
+        {
+            xPos = xp;
+            xPos = yp;
+        }
+
+ 
+        public virtual int getMyId();
+    }
+}
--- ibagchaal/Tiger.cs
+++ ibagchaal/Tiger.cs
@@ -5,52 +5,29 @@ using System.Text;
 
 namespace ibagchaal
 {
-    class Tiger
+    class Tiger:Player
     {
-        private BoardModel myParent;
-
+      
         public Tiger(int xP, int yP, BoardModel parent)
         {
-            xP = xPos;
-            yP = yPos;
-            myParent = parent;
+            Player(xP, yP, parent);
         }
 
         public bool isBlocked(int[,] board)
         {
             if (myParent.isPositionOccupied(xPos - 1, yPos - 1) || myParent.isPositionOccupied(xPos + 1, yPos + 1) ||
-                myParent.isPositionOccupied(xPos, yPos - 1) || myParent.isPositionOccupied(xPos + 1, yPos + 1))
+                myParent.isPositionOccupied(xPos, yPos - 1) || myParent.isPositionOccupied(xPos, yPos + 1) ||
+                myParent.isPositionOccupied(xPos-1, yPos ) || myParent.isPositionOccupied(xPos + 1, yPos))
                 return true;
             else
                 return false;
 
         }
 
-        public int getXPos()
-        {
-            return xPos;
-        }
-
-        public int getYPos()
-        {
-            return yPos;
-        }
-
-        public int setXPos(int xp)
-        {
-            xPos = xp;
-        }
-
-        public int setYPos(int yp)
-        {
-            yPos = yp;
-        }
 
         public int getMyId()
         {
             return BoardModel.TIGER;
         }
-
-        int xPos, yPos;
     }
 }
