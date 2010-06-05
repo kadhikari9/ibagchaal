1b81b1ee6c1153b69e530c5a9a01824318c7a3cc
dr kusu pro2000 <dr.kusu.pro2000@gmail.com@7b603418-8d72-82e7-18f8-41cf01e9d598>
2010-06-05 08:01:33 +0000

    Board Made.....Objective: Define different position, color, and size and suggest me.

---
+++ ibagchaal/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ibagchaal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            boardView = new BoardView(g);
+            boardView.drawBoard(500, 500, Color.Red, 5);
+        }
+
+        private Graphics g;         //to get graphics handler of this form that has already been created automatically.
+        private BoardView boardView;
+    }
+}
